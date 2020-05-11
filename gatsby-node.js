72aa56dc443b6bd8109d083f100fee13b198f342
Aditi <asaki444@gmail.com>
2020-05-11 18:00:56 -0400

    configured blog template to render blog information from read me files

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,10 +5,8 @@ exports.onCreateNode = ({
     actions
 }) => {
     const {
-        createNode,
         createNodeField
     } = actions
-    // Transform the new node here and create a new node or
 
 
     if (node.internal.type === "MarkdownRemark") {
@@ -19,5 +17,39 @@ exports.onCreateNode = ({
             value: slug
         })  
     }
+
+}
+
+
+exports.createPages = async ({graphql, actions}) => {
+    const{ createPage } = actions;
+    const blogTemplate = path.resolve('./src/templates/blog.js')
+    const res = await graphql(`query {
+        allMarkdownRemark {
+          edges {
+            node{
+              fields{
+                slug
+              }
+            }
+          }
+        }
+      }   
+    `
+    )
+
+    res.data.allMarkdownRemark.edges.forEach((edge)=>{
+        createPage({
+            component: blogTemplate,
+            path: `/blog/${edge.node.fields.slug}`,
+            context: {
+                slug: edge.node.fields.slug
+            }
+        })
+    })
+    // 1. Get path to template
+    // 2. Get markdown data
+    // 3. Create new pages
+
 
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,6 @@
 import React from 'React';
 import Layout from '../components/Layout';
-import {graphql, useStaticQuery } from 'gatsby';
+import {graphql, useStaticQuery, Link } from 'gatsby';
 
 
 const BlogPage = () => {
@@ -10,6 +10,9 @@ const BlogPage = () => {
         allMarkdownRemark {
           edges {
             node {
+              fields {
+                  slug
+              }
               frontmatter {
                 date
                 title
@@ -32,13 +35,14 @@ const BlogPage = () => {
                 {data.allMarkdownRemark.edges.map((edge)=>{
                     return(
                         <li>
+                          <Link to={`/blog/${edge.node.fields.slug}`}>
                             <h2>
                              {edge.node.frontmatter.title}
                             </h2>
                             <p>
                                 {edge.node.frontmatter.date}
                             </p>
-
+                            </Link>
                         </li>
                     )
                 })}
