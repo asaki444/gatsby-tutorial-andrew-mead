7bfff288135abf45534f7abd85bd1af05649e23d
Aditi <asaki444@gmail.com>
2020-05-14 22:05:37 -0400

    added contentful plugin, and fixed package.json to load environment variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,15 @@ module.exports = {
    title: "Aditi Develops",
    author: "Aditi G"
   },
-  plugins: ['gatsby-plugin-sass',
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    },
+    'gatsby-plugin-sass',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
