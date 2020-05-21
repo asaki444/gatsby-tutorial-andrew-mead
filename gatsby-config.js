dfdb771383cd39ccb7e533813d9fd3596f0ceead
Aditi <asaki444@gmail.com>
2020-05-20 23:08:51 -0400

    configured title for pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
    author: "Aditi G"
   },
   plugins: [
+    "gatsby-plugin-react-helmet",
     {
       resolve: 'gatsby-source-contentful',
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from '../components/Layout';
+import Head from '../components/Head';
 
 
 const IndexPage = ()=>{
   return (
       <Layout>
+        <Head title="Home" />
         <h1>
               Hello
           </h1>
